1b13ecf871cc37e0b7f4c05b77d19f82f7f03b9f
120241400 <89237995+120241400@users.noreply.github.com>
2022-11-13 13:21:51 +0800

    Add files via upload

---
+++ 7080/js/zy.js
@@ -0,0 +1,108 @@
+var appzy = {
+    "0": {
+        "name": "我爱跟剧",
+        "url": "https://www.genmov.com/mogai_api.php/v1.vod"
+    },
+    "1": {
+        "name": "QC影视",
+        "url": "https://www.qcsvip.com/mogai_api.php/v1.vod"
+    },
+    "2": {
+        "name": "看剧吧",
+        "url": "https://app.ishen520.com/api.php/v1.vod"
+    },
+    "3": {
+        "name": "小七影视",
+        "url": "https://www.xiaoqi0.cn/cuican_api.php/v1.vod"
+    },
+    "4": {
+        "name": "淘剧社",
+        "url": "https://app.shuhai99.com/api.php/v1.vod"
+    },
+    "5": {
+        "name": "兔子窝",
+        "url": "http://app.qianju.cc/api.php/v1.vod"
+    },
+    "6": {
+        "name": "佳佳影视",
+        "url": "https://www.jiajia.pub/api.php/v1.vod"
+    },
+    "7": {
+        "name": "龙猫影视",
+        "url": "https://lb.ybnxcx.com/api.php/v1.vod"
+    },
+    "8": {
+        "name": "爱酷影视",
+        "url": "https://www.zhanlangbu.com/ruifenglb_api.php/v1.vod"
+    },
+    "9": {
+        "name": "暖光视频",
+        "url": "https://app.bl210.com/api.php/v1.vod"
+    },
+    "10": {
+        "name": "寒枫影视",
+        "url": "https://vip.shenyunwl.cn/mogai_api.php/v1.vod"
+    }
+};
+var pingguozy = {
+    "0": {
+        "name": "三九",
+        "url": "https://www.39kan.com/api.php/provide/vod/"
+    },
+    "1": {
+        "name": "新浪",
+        "url": "https://api.xinlangapi.com/xinlangapi.php/provide/vod/"
+    },
+    "2": {
+        "name": "鱼乐",
+        "url": "https://api.yulecj.com/api.php/provide/vod/"
+    },
+    "3": {
+        "name": "天空",
+        "url": "https://api.tiankongapi.com/api.php/provide/vod"
+    },
+    "4": {
+        "name": "卧龙",
+        "url": "https://collect.wolongzyw.com/api.php/provide/vod/"
+    },
+    "5": {
+        "name": "百度",
+        "url": "https://api.apibdzy.com/api.php/provide/vod"
+    },
+    "6": {
+        "name": "快车",
+        "url": "https://caiji.kczyapi.com/api.php/provide/vod/"
+    },
+    "7": {
+        "name": "量子",
+        "url": "https://cj.lziapi.com/api.php/provide/vod"
+    },
+    "8": {
+        "name": "FOX",
+        "url": "https://api.foxzyapi.com/api.php/provide/vod/"
+    },
+    "9": {
+        "name": "酷点",
+        "url": "https://api.kuapi.cc/api.php/provide/vod/at/json/?https://kudian10.com/api.php/provide/vod"
+    },
+    "10": {
+        "name": "金鹰",
+        "url": "https://jyzyapi.com/provide/vod"
+    },
+    "11": {
+        "name": "木耳",
+        "url": "http://vip.muoutv.com/api.php/provide/vod/"
+    },
+    "12": {
+        "name": "速更",
+        "url": "https://sugengzy.cn/api.php/provide/vod/"
+    },
+    "13": {
+        "name": "光速",
+        "url": "https://api.guangsuapi.com/api.php/provide/vod/"
+    },
+    "14": {
+        "name": "U酷",
+        "url": "https://api.ukuapi.com/api.php/provide/vod"
+    }
+};
