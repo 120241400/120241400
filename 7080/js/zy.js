fea419a6330340d562d89c858f56f5efe29e8054
120241400 <89237995+120241400@users.noreply.github.com>
2023-03-20 15:23:20 +0800

    Update zy.js

--- 7080/js/zy.js
+++ 7080/js/zy.js
@@ -4,12 +4,12 @@ var appzy = {
         "url": "http://app.qianju.cc/api.php/v1.vod"
     },
     "1": {
-        "name": "188影视",
-        "url": "https://188kan.com/api.php/v1.vod"
+        "name": "聚合影视",
+        "url": "http://47.113.126.237:1234/api.php/provide/vod"
     },
     "2": {
-        "name": "天天影视",
-        "url": "http://app.qianju.cc/api.php/v1.vod"
+        "name": "188影视",
+        "url": "https://188kan.com/api.php/v1.vod"
     },
     "3": {
         "name": "筑梦影视",
