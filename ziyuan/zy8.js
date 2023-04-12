57150a81d6622819a18aa5c819959319f047f6a4
120241400 <89237995+120241400@users.noreply.github.com>
2023-04-12 13:00:49 +0800

    Add files via upload

---
+++ ziyuan/zy8.js
@@ -0,0 +1,36 @@
+var pingguozy= {
+    "0": {
+        "name": "非凡", "url":"http://ffzy5.tv/api.php/provide/vod/from/ffm3u8", "play":"./jx/index.html?url="
+    }
+    ,
+    "1": {
+        "name": "新浪", "url":"https://api.xinlangapi.com/xinlangapi.php/provide/vod/", "play":"./jx/index.html?url="
+    }
+    ,
+    "2": {
+        "name": "鱼乐", "url":"https://api.yulecj.com/api.php/provide/vod/from/lem3u8", "play":"./jx/index.html?url="
+    }
+    ,
+    "3": {
+        "name": "红牛", "url":"https://www.hongniuzy2.com/api.php/provide/vod/from/hnm3u8", "play":"./jx/index.html?url="
+    }
+    ,
+    "4": {
+        "name": "卧龙", "url":"https://collect.wolongzyw.com/api.php/provide/vod/from/ffm3u8", "play":"./jx/index.html?url="
+    }
+    ,
+    "5": {
+        "name": "快车", "url":"https://caiji.kczyapi.com/api.php/provide/vod/from/ffm3u8", "play":"./jx/index.html?url="
+    }
+    ,
+    "6": {
+        "name": "酷点", "url":"https://api.kuapi.cc/api.php/provide/vod/at/json/", "play":"./jx/index.html?url="
+    }
+    ,
+    "7": {
+        "name": "光速", "url":"https://api.guangsuapi.com/api.php/provide/vod/from/ffm3u8", "play":"./jx/index.html?url="
+    }
+}
+;
+
+
