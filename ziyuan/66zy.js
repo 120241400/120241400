e0de43a1939a553804c213169d78ba99c111bef5
120241400 <89237995+120241400@users.noreply.github.com>
2023-05-04 09:52:30 +0800

    Add files via upload

---
+++ ziyuan/66zy.js
@@ -0,0 +1,80 @@
+var pingguozy= {
+    "0": {
+        "name": "非凡", "url":"http://ffzy5.tv/api.php/provide/vod/from/ffm3u8", "play":"./jx/index.html?url="
+    }
+    ,
+    "1": {
+        "name": "新浪", "url":"https://api.xinlangapi.com/xinlangapi.php/provide/vod/from/xlm3u8", "play":"./jx/index.html?url="
+    }
+    ,
+    "2": {
+        "name": "鱼乐", "url":"https://api.yulecj.com/api.php/provide/vod/from/lem3u8", "play":"./jx/index.html?url="
+    }
+    ,
+    "3": {
+        "name": "红牛", "url":"https://www.hongniuzy2.com/api.php/provide/vod/from/hnm3u8", "play":"./jx/index.html?url="
+    }
+    ,
+    "4": {
+        "name": "卧龙", "url":"https://collect.wolongzyw.com/api.php/provide/vod/", "play":"./jx/index.html?url="
+    }
+    ,
+    "5": {
+        "name": "快车", "url":"https://caiji.kczyapi.com/api.php/provide/vod/from/kcm3u8", "play":"./jx/index.html?url="
+    }
+    ,
+    "6": {
+        "name": "无尽", "url":"https://api.wujinapi.com/api.php/provide/vod/from/wjm3u8", "play":"./jx/index.html?url="
+    }
+    ,
+    "7": {
+        "name": "光速", "url":"https://api.guangsuapi.com/api.php/provide/vod/from/gsm3u8", "play":"./jx/index.html?url="
+    }
+}
+;
+
+var appzy= {
+    "0": {
+        "name": "123看", "url":"https://123aktv.com/api.php/v1.vod"
+    }
+    ,
+    "1": {
+        "name": "看剧影视", "url":"https://app.ishen520.com/api.php/v1.vod"
+    }
+    ,
+    "2": {
+        "name": "霹雳影视", "url":"https://plyy.cc/api.php/v1.vod"
+    }
+    ,
+    "3": {
+        "name": "188看", "url":"https://www.188kan.com/api.php/v1.vod"
+    }
+    ,
+    "4": {
+        "name": "影库", "url":"https://yingkudy.com/lvdou_api.php/v1.vod"
+    }
+    ,
+    "5": {
+        "name": "免费影视", "url":"https://wlgwys.com/api.php/v1.vod"
+    }
+}
+;
+
+var tjzy= {
+    "0": {
+        "name": "爱看", "url":"https://www.ikanbot.com/search?q=", "pic":"https://s1.ax1x.com/2023/05/04/p9YOe4P.jpg"
+    }
+    ,
+    "1": {
+        "name": "邦宁云搜", "url":"https://video.isyour.love/Search/SearchName/", "pic":"https://s1.ax1x.com/2023/05/04/p9YOMjg.jpg"
+    }
+    ,
+    "2": {
+        "name": "360影视", "url":"https://baidu.120241400.repl.co/list/search/?keyword=", "pic":"https://s1.ax1x.com/2023/05/04/p9YOGEn.md.jpg"
+    }
+    ,
+    "3": {
+        "name": "M1907", "url":"https://z1.m1907.top/?jx=", "pic":"https://s1.ax1x.com/2023/05/04/p9YOU3T.jpg"
+    }
+}
+;
