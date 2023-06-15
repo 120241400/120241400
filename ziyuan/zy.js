3180698661a15e4871bfd60185b08de4d50293a4
120241400 <89237995+120241400@users.noreply.github.com>
2023-06-16 06:01:12 +0800

    Update zy.js

--- ziyuan/zy.js
+++ ziyuan/zy.js
@@ -4,7 +4,7 @@ var pingguozy= {
     }
     ,
     "1": {
-        "name": "大漠", "url":"https://damozy.com/api.php/provide/vod/from/M3U8"
+        "name": "速播", "url":"https://subocaiji.com/api.php/provide/vod/from/subm3u8/"
     }
     ,
     "2": {
