c34c9d3c4c1a0f3273d66c9cbb65b5a65fa878a0
120241400 <89237995+120241400@users.noreply.github.com>
2023-09-12 07:43:55 +0800

    Update zy.js

--- ziyuan/zy.js
+++ ziyuan/zy.js
@@ -1,6 +1,6 @@
 var pingguozy= {
     "0": {
-        "name": "极速", "url":"https://jszyapi.com/api.php/provide/vod/from/jsm3u8/", "play":"./jx/index.html?url="
+        "name": "高清", "url":"https://api.1080zyku.com/inc/apijson.php", "play":"./jx/index.html?url="
     }
     ,
     "1": {
